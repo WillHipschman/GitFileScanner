1e70940eb8ce5562aa982617a27e75045816557f
William Hipschman <willhi@microsoft.com>
2015-12-21 15:09:29 -0500

    Continuing Installer Work. CLI Parsin

--- Install.rb
+++ Install.rb
@@ -1,8 +1,35 @@
-require 'pathname'
 
-def Install(path)
+@usage = "usage: Install.rb <repository path>"
 
+def PrintUsage(stream)
+    stream.puts(@usage)
 end
 
+def IsValidGitFolder(path)
+    gitFolder = path + "\\.git"
+    return File.exist?(gitFolder)
+end
 
-ARGF.each{|path|Install path}
+def Quit(message)
+    $stderr.puts("#{path} is not a valid git repository.\n\n")
+    PrintUsage($stderr)
+    abort
+end
+
+def Install(path)
+    if File.exist?(path)
+        if IsValidGitFolder(path)
+            puts("SUCCESS")
+        else
+            Quit("#{path} is not a valid git repository.\n\n")
+        end
+    else
+        Quit("The path #{path} does not exist.\n\n")
+    end
+end
+
+if(ARGV.length != 1)
+    Quit("Invalid number of arguments.\n\n")
+end
+
+Install(ARGV.first)
